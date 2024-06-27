cf9e26969260b1afd54dcbf6746600bbbb22f273
brayant <blopez@tailoredalloys.com>
2024-06-27 10:21:22 -0400

    updated

--- myproject/startup.sh
+++ myproject/startup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
-python manage.py collectstatic && gunicorn --workers 2 myproject.wsgi -b 0.0.0.0:8000
+#!/bin/bash
+python3 manage.py collectstatic && gunicorn --workers 2 myproject.wsgi -b 0.0.0.0:8000
  
  
